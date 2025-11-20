7e190be8b6f91118298d751e94d9c3f4f5b05bca
Jack O'Connor <oconnor663@gmail.com>
2025-11-19 21:24:21 -0800

    streams example

--- examples/example.rs
+++ examples/example.rs
@@ -1,19 +1,26 @@
+use futures::StreamExt;
 use std::io::prelude::*;
+use tokio_stream::wrappers::IntervalStream;
 
 #[tokio::main]
 async fn main() {
-    let mut stream1 = tokio::time::interval(std::time::Duration::from_millis(100));
-    let mut stream2 = tokio::time::interval(std::time::Duration::from_millis(301));
-    loop {
-        tokio::select!(
-            _ = stream1.tick() => {
-                print!("1");
-                std::io::stdout().flush().unwrap();
-            },
-            _ = stream2.tick() => {
-                print!("2");
-                std::io::stdout().flush().unwrap();
-            },
-        );
-    }
+    let stream1 = IntervalStream::new(tokio::time::interval(std::time::Duration::from_millis(100)));
+    let stream2 = IntervalStream::new(tokio::time::interval(std::time::Duration::from_millis(301)));
+    let stream3 = IntervalStream::new(tokio::time::interval(std::time::Duration::from_secs(1)));
+    futures::join!(
+        stream1.for_each(|_| async {
+            print!("1");
+            std::io::stdout().flush().unwrap();
+        }),
+        stream2.for_each(|_| async {
+            print!("2");
+            std::io::stdout().flush().unwrap();
+        }),
+        stream3.for_each(|_| async {
+            println!("3");
+            if rand::random_ratio(1, 5) {
+                tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
+            }
+        }),
+    );
 }
