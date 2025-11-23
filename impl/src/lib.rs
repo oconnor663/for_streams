ed27ff7916b3c8d1cb4665c3cd5e80f762e7f609
Jack O'Connor <oconnor663@gmail.com>
2025-11-22 22:06:37 -0800

    be less verbose with identifier variables

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -8,7 +8,7 @@ use syn::{
 
 struct Arm {
     pattern: Pat,
-    stream: Expr,
+    stream_expr: Expr,
     body: Block,
     is_move: bool,
 }
@@ -17,13 +17,13 @@ impl Parse for Arm {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let pattern = Pat::parse_single(input)?;
         _ = input.parse::<Token![in]>()?;
-        let stream = input.parse()?;
+        let stream_expr = input.parse()?;
         _ = input.parse::<Token![=>]>()?;
         let is_move = input.parse::<Token![move]>().is_ok();
         let body = input.parse()?;
         Ok(Self {
             pattern,
-            stream,
+            stream_expr,
             body,
             is_move,
         })
@@ -47,12 +47,12 @@ impl Parse for ForStreams {
 impl ToTokens for ForStreams {
     fn to_tokens(&self, tokens: &mut TokenStream2) {
         let ForStreams { arms } = self;
-        let cancel_sender_ident = format_ident!("cancel_sender", span = Span::mixed_site());
+        let cancel_sender = format_ident!("cancel_sender", span = Span::mixed_site());
         let mut arm_tokens = Vec::new();
         for arm in arms {
             let Arm {
                 pattern,
-                stream,
+                stream_expr,
                 body,
                 is_move,
             } = arm;
@@ -61,48 +61,45 @@ impl ToTokens for ForStreams {
             } else {
                 quote! {}
             };
-            let returned_early_ident = format_ident!("returned_early", span = Span::mixed_site());
-            let returned_early_ref_ident =
-                format_ident!("returned_early_ref", span = Span::mixed_site());
-            let stream_ident = format_ident!("stream", span = Span::mixed_site());
+            let returned_early = format_ident!("returned_early", span = Span::mixed_site());
+            let returned_early_ref = format_ident!("returned_early_ref", span = Span::mixed_site());
+            let stream = format_ident!("stream", span = Span::mixed_site());
             arm_tokens.push(quote! {
                 {
-                    let #cancel_sender_ident = ::futures::channel::mpsc::Sender::clone(&#cancel_sender_ident);
+                    let #cancel_sender = ::futures::channel::mpsc::Sender::clone(&#cancel_sender);
                     async {
-                        let mut #cancel_sender_ident = #cancel_sender_ident;
-                        let mut #returned_early_ident = true;
+                        let mut #cancel_sender = #cancel_sender;
+                        let mut #returned_early = true;
                         // For the `move` case, we need to explicitly take a reference to
                         // `returned_early`, so that we don't copy it.
-                        let #returned_early_ref_ident = &mut #returned_early_ident;
+                        let #returned_early_ref = &mut #returned_early;
                         let _: () = async #move_token {
-                            let mut #stream_ident = ::std::pin::pin!(#stream);
-                            while let Some(#pattern) = ::futures::stream::StreamExt::next(&mut #stream_ident).await {
+                            let mut #stream = ::std::pin::pin!(#stream_expr);
+                            while let Some(#pattern) = ::futures::stream::StreamExt::next(&mut #stream).await {
                                 // NOTE: The #body may `continue`, `break`, or `return`.
                                 #body
                             }
-                            *#returned_early_ref_ident = false;
+                            *#returned_early_ref = false;
                         }.await;
-                        if #returned_early_ident {
-                            _ = ::futures::sink::SinkExt::send(&mut #cancel_sender_ident, ()).await;
+                        if #returned_early {
+                            _ = ::futures::sink::SinkExt::send(&mut #cancel_sender, ()).await;
                         }
                     }
                 }
             });
         }
-        let cancel_receiver_ident = format_ident!("cancel_receiver", span = Span::mixed_site());
-        let joined_arms_ident = format_ident!("joined_arms", span = Span::mixed_site());
+        let cancel_receiver = format_ident!("cancel_receiver", span = Span::mixed_site());
         tokens.extend(quote! {
             {
                 // TODO: Support `return` without heap allocation somehow.
-                let (#cancel_sender_ident, mut #cancel_receiver_ident) = ::futures::channel::mpsc::channel::<()>(0);
-                let #joined_arms_ident = async {
-                    ::futures::join! {
-                        #(#arm_tokens),*
-                    }
-                };
+                let (#cancel_sender, mut #cancel_receiver) = ::futures::channel::mpsc::channel::<()>(0);
                 ::for_streams::_race(
-                    #joined_arms_ident,
-                    ::futures::stream::StreamExt::next(&mut #cancel_receiver_ident),
+                    async {
+                        ::futures::join! {
+                            #(#arm_tokens),*
+                        }
+                    },
+                    ::futures::stream::StreamExt::next(&mut #cancel_receiver),
                 ).await;
             }
         });
