d331d84a71bcbdd7ba8bbd06127a011b10234464
Jack O'Connor <oconnor663@gmail.com>
2025-11-26 22:32:33 -0800

    EXPERIMENT: join_me_maybe!

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,12 +1,25 @@
 //! The main purpose of this example is to have something minimal to `cargo expand` when I'm
 //! debugging compiler errors in the macro.
 
-use for_streams::for_streams;
+use for_streams::join_me_maybe;
+use std::time::Duration;
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    let stream = futures::stream::iter(0..5);
-    for_streams! {
-        _ in stream => {}
-    }
+    dbg!(join_me_maybe! {
+        async definitely {
+            for _ in 0..3 {
+                sleep(Duration::from_secs(1)).await;
+                println!("definitely!");
+            }
+            42
+        }
+        async maybe {
+            loop {
+                sleep(Duration::from_millis(300)).await;
+                println!("maybe?");
+            }
+        }
+    });
 }
